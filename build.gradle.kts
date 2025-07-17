d0fb90e77831a892e7755cccc4261d49e2fa0ab2
Oleksandr Kolodkin <oleksandr.kolodkin@ukr.net>
2025-07-17 20:22:48 +0300

    Update versions:  - kotlin 1.9.20 -> 2.2.0  - shadow 8.1.1 -> 8.3.8  - slf4j 2.0.12 -> 2.0.17  - log4j 2.22.1 -> 2.25.1  - flatlaf 3.3 -> 3.6.1  - fontchooser 2.5.2 -> 3.1.0  - sqlite-jdbc 3.45.1.0 -> 3.50.2.0
    Signed-off-by: Oleksandr Kolodkin <oleksandr.kolodkin@ukr.net>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import java.text.SimpleDateFormat
 import java.util.Date
 
 plugins {
     application
-    kotlin("jvm") version "1.9.20"
-    kotlin("kapt") version "1.9.20"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    kotlin("jvm") version "2.2.0"
+    kotlin("kapt") version "2.2.0"
+    id("com.gradleup.shadow") version "8.3.8"
 }
 
 project.ext {
@@ -23,17 +23,17 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.20")
-    implementation("org.slf4j:slf4j-log4j12:2.0.12")
-    implementation("org.apache.logging.log4j:log4j-core:2.22.1")
-    implementation("org.apache.logging.log4j:log4j-api:2.22.1")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.22.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.2.0")
+    implementation("org.slf4j:slf4j-log4j12:2.0.17")
+    implementation("org.apache.logging.log4j:log4j-core:2.25.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.25.1")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.25.1")
     implementation("io.github.kolod:flatlaf-themes-combobox-model:1.1.1")
-    implementation("com.formdev:flatlaf:3.3")
-    implementation("com.formdev:flatlaf-extras:3.3")
-    implementation("com.formdev:flatlaf-intellij-themes:3.3")
-    implementation("org.drjekyll:fontchooser:2.5.2")
-    implementation("org.xerial:sqlite-jdbc:3.45.1.0")
+    implementation("com.formdev:flatlaf:3.6.1")
+    implementation("com.formdev:flatlaf-extras:3.6.1")
+    implementation("com.formdev:flatlaf-intellij-themes:3.6.1")
+    implementation("org.drjekyll:fontchooser:3.1.0")
+    implementation("org.xerial:sqlite-jdbc:3.50.2.0")
     implementation("com.jcabi:jcabi-manifests:2.1.0")
 }
 
@@ -46,11 +46,9 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "1.8"
-        }
-    }
+kotlin {
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_1_8)
+}
 
 tasks.named<ShadowJar>("shadowJar") {
     manifest {
